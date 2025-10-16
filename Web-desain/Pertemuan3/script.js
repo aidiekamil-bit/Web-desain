7cc96de19a506c0f814c2a44d25f14a14610c379
Aidielkamil <aidiekamil@gmail.com>
2025-10-16 09:13:27 +0700

    Add files via upload

---
+++ Web-desain/Pertemuan3/script.js
@@ -0,0 +1,17 @@
+const semua_tombol = document.querySelectorAll(".btn");
+const semua_gambar = document.querySelectorAll(".gambar");
+
+semua_tombol.forEach((tombol_satuan) => {
+    tombol_satuan.addEventListener("click", () => {
+    semua_tombol.forEach((b) => b.classList.remove("active"));
+    tombol_satuan.classList.add("active");
+    const filter = tombol_satuan.getAttribute("data-filter");
+    semua_gambar.forEach((gambar_satuan) => {
+        if (filter === "all" || gambar_satuan.classList.contains(filter)) {
+            gambar_satuan.style.display = "block";
+        } else {
+            gambar_satuan.style.display = "none";
+        }
+    });
+    });
+});
